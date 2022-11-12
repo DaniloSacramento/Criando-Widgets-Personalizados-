97e61574d58ec93e7919541dffd0b644c22d4c8c
Danilo Sacramento <89158678+DaniloSacramento@users.noreply.github.com>
2022-11-11 23:49:00 -0300

    Utilizando gerenciador de estado nativo

--- personalizado/lib/one_page.dart
+++ personalizado/lib/one_page.dart
@@ -1,20 +1,48 @@
+import 'dart:math';
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/container.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:personalizado/widgets/custom_button_widget.dart';
 
-class OnePage extends StatelessWidget {
+class OnePage extends StatefulWidget {
   const OnePage({super.key});
+
+  @override
+  State<OnePage> createState() => _OnePageState();
+}
+
+class _OnePageState extends State<OnePage> {
+
+  ValueNotifier<int> valorAleatorio = ValueNotifier<int> (0);
+
+  random() async{
+      for(int i = 0; i < 10; i++ ){ 
+        await Future.delayed(Duration(seconds: 1));
+         
+           valorAleatorio.value = Random().nextInt(1000);
+     
+      }
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: CustomButtomWidget(
-          disable: false,
-          onPressed: (){}, 
-          title: "CustomButtom",
-          titleSize: 20,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            ValueListenableBuilder(valueListenable: valorAleatorio, builder: (_, valor, __) => Text(
+            "Valor eh : $valor",
+            style: const TextStyle(fontSize: 20),)
+            ),
+          const SizedBox(height: 10,),
+            CustomButtomWidget(
+              disable: false,
+              onPressed: () => random(), 
+              title: "CustomButtom",
+              titleSize: 20,
+            ),
+          ],
         ),
       ),
     );
