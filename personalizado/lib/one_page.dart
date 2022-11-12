f974649ebdd33ed1c45b93faac9d27b2a016bf0c
Danilo Sacramento <89158678+DaniloSacramento@users.noreply.github.com>
2022-11-12 00:54:12 -0300

    consumindo api

--- personalizado/lib/one_page.dart
+++ personalizado/lib/one_page.dart
@@ -1,8 +1,9 @@
-import 'dart:math';
+import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/container.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:personalizado/widgets/custom_button_widget.dart';
+import 'package:http/http.dart' as http;
 
 class OnePage extends StatefulWidget {
   const OnePage({super.key});
@@ -12,39 +13,74 @@ class OnePage extends StatefulWidget {
 }
 
 class _OnePageState extends State<OnePage> {
+  ValueNotifier<List<Post>> posts  = ValueNotifier<List<Post>>([]);
 
-  ValueNotifier<int> valorAleatorio = ValueNotifier<int> (0);
+  callAPT() async {
+    var client = http.Client();
+    try {
+      var response = await client.get
+      (Uri.parse('https://jsonplaceholder.typicode.com/posts')
+      );
 
-  random() async{
-      for(int i = 0; i < 10; i++ ){ 
-        await Future.delayed(Duration(seconds: 1));
-         
-           valorAleatorio.value = Random().nextInt(1000);
-     
-      }
+      var decodedResponse = jsonDecode(response.body) as List;
+      
+      posts.value = decodedResponse.map((e) => Post.fromJson(e)).toList()  ;
+      
+    } finally {
+      client.close();
+    }
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            ValueListenableBuilder(valueListenable: valorAleatorio, builder: (_, valor, __) => Text(
-            "Valor eh : $valor",
-            style: const TextStyle(fontSize: 20),)
-            ),
-          const SizedBox(height: 10,),
-            CustomButtomWidget(
-              disable: false,
-              onPressed: () => random(), 
-              title: "CustomButtom",
-              titleSize: 20,
-            ),
-          ],
+        child: SingleChildScrollView(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              ValueListenableBuilder<List<Post>>(
+                  valueListenable: posts ,
+                  builder: (_, valor, __) => ListView.builder(
+                    physics: const NeverScrollableScrollPhysics(),
+                    shrinkWrap: true,
+                    itemCount: valor.length,
+                    itemBuilder: (_, idx) => ListTile(title: Text(valor[idx].title)
+                    ),
+                  )
+                      ),
+              const SizedBox(
+                height: 10,
+              ),
+              CustomButtomWidget(
+                disable: false,
+                onPressed: () => callAPT(),
+                title: "CustomButtom",
+                titleSize: 20,
+              ),
+            ],
+          ),
         ),
       ),
     );
   }
 }
+
+
+class Post { 
+  final int userId;
+  final int id;
+  final String title;
+  final String body;
+
+  Post(this.userId, this.id, this.title, this.body);
+
+  factory Post.fromJson(Map json) {
+      return Post(json["userId"],json["id"],json["title"],json["body"]);
+  }
+  @override
+  String toString() {
+    return "id: $id";
+  }
+}
+
